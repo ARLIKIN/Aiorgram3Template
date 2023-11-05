816484a9dc3e8b423abea944240655638c9f1654
ARLIKIN <38999833+ARLIKIN@users.noreply.github.com>
2023-11-05 17:51:44 +0400

    Update inline.py

--- bot/keyboards/inline.py
+++ bot/keyboards/inline.py
@@ -3,6 +3,8 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 def get_yes_no_kb() -> InlineKeyboardMarkup:
-    kb = InlineKeyboardBuilder()
-    kb.button(InlineKeyboardButton(text='', callback_data=''))
-    return kb.as_markup(resize_keyboard=True)
+   kb = InlineKeyboardBuilder()
+        kb.button(text='Да', callback_data='none')
+        kb.button(text='Нет', callback_data='none')
+    kb.adjust(1)
+    return kb.as_markup()
